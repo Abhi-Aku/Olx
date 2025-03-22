43b9ce6dcc45d107eaa81e535ab8fb72d21e966a
Abhi-Aku <abhi2003av@gmail.com>
2025-03-22 14:51:52 +0530

    dataUpdate
    dataUpdate

--- OlxClone/src/Pages/Mobile.jsx
+++ OlxClone/src/Pages/Mobile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import Prodect from '../Pages/Prodect'
 const Mobile = () => {
   return (
-    <>
+  
+     <>
       <div className='flex  items-center px-5'>
         <caption>Popular Searches:
           <NavLink className='hover:text-blue-300' to="/search/iphone">iphone</NavLink > - 
@@ -16,8 +17,28 @@ const Mobile = () => {
           <NavLink className='hover:text-blue-300' to="/search/mi">mi</NavLink > - 
           <NavLink className='hover:text-blue-300' to="/search/motorola">motorola</NavLink>
         </caption>
-      </div>    
-    </>
+      </div> 
+      <br></br>
+      <div className="px-4 py-6 md:px-8">
+      <nav className="text-sm text-gray-500">
+        <a href="/" className="hover:underline">Home</a> / <a href="#" className="hover:underline">Mobiles</a>
+      </nav>
+      <h1 className="mt-2 text-2xl font-bold text-gray-900 md:text-3xl">
+        Buy & Sell Second Hand Mobile Phones in India
+      </h1>
+      <p className="mt-2 text-gray-600 ">
+        Explore a vast collection of  looking for the latest
+        flagship smartphones or affordable budget-friendly options, OLX has a wide selection from top brands. Our listings…
+        <a href="#" className="text-blue-600 font-medium hover:underline"> Read more</a>
+      </p>
+    </div>
+    <Prodect/>
+      
+    
+   
+       
+     </>   
+  
   );
 }
 
